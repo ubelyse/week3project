127d9ae2278a9972f81489692e8dd27efe0d1468
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-10-05 13:08:25 +0200

    added script

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
     $(".clickable").click(function() {
       $("#walrus-showing1").toggle();
       $("#walrus-hidden1").toggle();
-    
+
+      $("#walrus-showing2").toggle();
+      $("#walrus-hidden2").toggle();
+
+      $("#walrus-showing3").toggle();
+      $("#walrus-hidden3").toggle();
     })
   });
