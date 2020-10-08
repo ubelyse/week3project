4ed7cfaa829c0159430694589aa9bc092ec23775
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-10-08 14:59:19 +0200

    changed my form

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ $(document).ready(function() {
       $("#walrus-hidden3").toggle();
     })
   });
+  
+  $("form#contactform").on('submit',function(event){
+    event.preventDefault();
+    let name = $("input#name").val();
+    let email = $("input#email").val();
+    let message = $("textarea#message").val();
+
+    if ($("input#name").val() && $("input#email").val()){
+        alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...cheers");
+    }
+    else {
+        alert("Please provide your correct name and email!");
+    }
+
+});
